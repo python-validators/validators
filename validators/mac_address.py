40c249de3fd0571ab3a9180e7950d1f163fd1fae
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-10-21 11:59:31 +0300

    Added mac address validator

---
+++ validators/mac_address.py
@@ -0,0 +1,27 @@
+import re
+from .utils import validator
+
+
+pattern = re.compile(r'^(?:[0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$')
+
+
+@validator
+def mac_address(value):
+    """
+    Returns whether or not given value is a valid mac address. If the value is
+    valid mac address this function returns True, otherwise `FailedValidation`.
+
+    Examples::
+
+        >>> import validators
+
+        >>> validators.mac_address('01:23:45:67:ab:CD')
+        True
+        >>> validators.mac_address('00:00:00:00:00')
+        False
+
+    .. versionadded:: 0.2
+
+    :param value: Mac address string to validate
+    """
+    return pattern.match(value)
