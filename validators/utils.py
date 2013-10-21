ff6e237d31c3b2910a5841900eef02de2f6ccbc2
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-10-21 13:21:50 +0300

    Fixed compatibility issue with py3

--- validators/utils.py
+++ validators/utils.py
@@ -31,7 +31,7 @@ def func_args_as_dict(func, args, kwargs):
     )
     return OrderedDict(
         list(six.moves.zip(arg_names, args)) +
-        list(kwargs.iteritems())
+        list(kwargs.items())
     )
 
 
