37375fe28cef42ce6415b9eb91fc5a595cdbe6cc
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-10-23 09:01:40 +0300

    Added total_ordering to Python 2.6 requirements

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ setup(
         'six>=1.4.0',
         'decorator>=3.4.0',
         'ordereddict>=1.1'
+        if sys.version_info[0] == 2 and sys.version_info[1] < 7 else '',
+        'total_ordering>=0.1'
         if sys.version_info[0] == 2 and sys.version_info[1] < 7 else ''
     ],
     extras_require=extras_require,
