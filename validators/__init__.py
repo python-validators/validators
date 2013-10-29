05c35b05f66ce74d8a9d54dc48c4b45e7ec206e4
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-10-29 13:03:37 +0200

    Added finnish ssn validator

--- validators/__init__.py
+++ validators/__init__.py
@@ -1,7 +1,6 @@
 from .between import between
 from .email import email
 from .extremes import Min, Max
-from .finnish_business_id import finnish_business_id
 from .ip_address import ipv4, ipv6
 from .length import length
 from .mac_address import mac_address
@@ -9,12 +8,14 @@ from .truthy import truthy
 from .url import url
 from .utils import ValidationFailure, validator
 from .uuid import uuid
+from .i18n import finnish_business_id, finnish_ssn
 
 
 __all__ = (
     between,
     email,
     finnish_business_id,
+    finnish_ssn,
     ipv4,
     ipv6,
     length,
---
+++ validators/i18n/__init__.py
@@ -0,0 +1,7 @@
+from .fi import finnish_ssn, finnish_business_id
+
+
+__all__ = (
+    finnish_ssn,
+    finnish_business_id,
+)
