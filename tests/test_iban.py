daf0b6fa1cd6eb08672508103c15d3e58c3471d1
Kees Hink <keeshink@gmail.com>
2015-06-22 17:42:29 +0200

    Added new validator: iban

---
+++ tests/test_iban.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pytest
+import validators
+
+
+@pytest.mark.parametrize(('value',), [
+    ('GB82WEST12345698765432',),
+])
+def test_returns_true_on_valid_iban(value):
+    assert validators.iban(value)
+
+
+@pytest.mark.parametrize(('value',), [
+    ('GB81WEST12345698765432',),
+])
+def test_returns_failed_validation_on_invalid_iban(value):
+    result = validators.iban(value)
+    assert isinstance(result, validators.ValidationFailure)
