5974870b202c6e65d65b20f5059113ad33a1f99b
shirizaan <shirizaan@gmail.com>
2016-12-20 11:13:00 -0700

    Added tests for image URLs.

---
+++ tests/test_image.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from validators import image, ValidationFailure
+
+#https://i.imgur.com/DKUR9Tk.png is Grumpy Cat
+
+@pytest.mark.parameterize('address', [
+    u'https://i.imgur.com/DKUR9Tk.png',
+    u'https://i.imgur.com/DKUR9Tk.png?fb'
+])
+def test_returns_true_on_valid_image_url(address):
+    assert image(path)
+
+@pytest.mark.parameterize('address', [
+    u'http://www.google.com/'
+])
+def test_returns_false_on_invalid_image_url(address):
+    assert isinstance(image(address), ValidationFailure)
