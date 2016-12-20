900a0ea176af0ecda3706ac3b8bc7139d7c221af
shirizaan <shirizaan@gmail.com>
2016-12-20 11:38:40 -0700

    Corrected typo.

--- tests/test_image.py
+++ tests/test_image.py
@@ -5,14 +5,16 @@ from validators import image, ValidationFailure
 
 #https://i.imgur.com/DKUR9Tk.png is Grumpy Cat
 
-@pytest.mark.parameterize('address', [
+
+@pytest.mark.parametrize('address', [
     u'https://i.imgur.com/DKUR9Tk.png',
     u'https://i.imgur.com/DKUR9Tk.png?fb'
 ])
 def test_returns_true_on_valid_image_url(address):
     assert image(path)
 
-@pytest.mark.parameterize('address', [
+
+@pytest.mark.parametrize('address', [
     u'http://www.google.com/'
 ])
 def test_returns_false_on_invalid_image_url(address):
