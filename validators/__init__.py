eb2cb386c5affdb9190e6bd84b3b921db6639fd3
shirizaan <shirizaan@gmail.com>
2016-12-21 00:22:30 -0700

    tests pass. imports sorted. isort passes.

--- validators/__init__.py
+++ validators/__init__.py
@@ -4,7 +4,7 @@ from .email import email  # noqa
 from .extremes import Max, Min  # noqa
 from .i18n import fi_business_id, fi_ssn  # noqa
 from .iban import iban  # noqa
-from .image import image # noqa
+from .image import image  # noqa
 from .ip_address import ipv4, ipv6  # noqa
 from .length import length  # noqa
 from .mac_address import mac_address  # noqa
