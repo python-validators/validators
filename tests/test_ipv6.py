ee5d5dbcef4de0f84f0ac26c98d0691c1a06bf69
Walter Purcaro <vuolter@users.noreply.github.com>
2017-06-01 13:41:36 +0200

    Fix typo in test_ipv6.py

--- tests/test_ipv6.py
+++ tests/test_ipv6.py
@@ -9,7 +9,7 @@ from validators import ipv6, ValidationFailure
     ('dead:beef:0:0:0:0:42:1',),
     ('abcd:ef::42:1',),
     ('0:0:0:0:0:ffff:1.2.3.4',),
-    ('::192.168.30.2'),
+    ('::192.168.30.2',),
 ])
 def test_returns_true_on_valid_ipv6_address(address):
     assert ipv6(address)
