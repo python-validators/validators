e9c2ed1954288ed7fd1733458041c69814bed10d
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2017-06-03 17:02:44 +0300

    Fix typos and styling

--- validators/hashes.py
+++ validators/hashes.py
@@ -29,7 +29,7 @@ def md5(value):
     """
     Return whether or not given value is a valid MD5 hash.
 
-    Exemples::
+    Examples::
 
         >>> md5('d41d8cd98f00b204e9800998ecf8427e')
         True
@@ -47,7 +47,7 @@ def sha1(value):
     """
     Return whether or not given value is a valid SHA1 hash.
 
-    Exemples::
+    Examples::
 
         >>> sha1('da39a3ee5e6b4b0d3255bfef95601890afd80709')
         True
@@ -65,7 +65,7 @@ def sha224(value):
     """
     Return whether or not given value is a valid SHA224 hash.
 
-    Exemples::
+    Examples::
 
         >>> sha224('d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f')
         True
@@ -83,9 +83,12 @@ def sha256(value):
     """
     Return whether or not given value is a valid SHA256 hash.
 
-    Exemples::
+    Examples::
 
-        >>> sha256('e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+        >>> sha256(
+        ...     'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b'
+        ...     '855'
+        ... )
         True
 
         >>> sha256('900zz11')
@@ -101,9 +104,13 @@ def sha512(value):
     """
     Return whether or not given value is a valid SHA512 hash.
 
-    Exemples::
+    Examples::
 
-        >>> sha512('cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e')
+        >>> sha512(
+        ...     'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce'
+        ...     '9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af9'
+        ...     '27da3e'
+        ... )
         True
 
         >>> sha512('900zz11')
