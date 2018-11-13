9e54c721882e10c75694be32a24df8695ea1eebd
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2018-11-13 14:09:18 +0200

    Improve fi-ssn validator (#97)
    * Fi-ssn validator: checksum must be upper case
    
    * Fi-ssn validator: better validation for the date
    
    * Fi-ssn validator: Implement serial validation
    
    Make fi-ssn validator by default accept all valid serials, meaning
    serials in range 2 - 999. Also add an option to not allow temporal
    serials that are in the range 900 - 999.

--- tests/i18n/test_fi.py
+++ tests/i18n/test_fi.py
@@ -25,9 +25,11 @@ def test_returns_failed_validation_on_invalid_email(value):
 
 
 @pytest.mark.parametrize(('value',), [
+    ('010190-002R',),
     ('010101-0101',),
     ('010101+0101',),
     ('010101A0101',),
+    ('010190-900P',),
 ])
 def test_returns_true_on_valid_ssn(value):
     assert fi_ssn(value)
@@ -36,6 +38,11 @@ def test_returns_true_on_valid_ssn(value):
 @pytest.mark.parametrize(('value',), [
     (None,),
     ('',),
+    ('010190-001P',),  # Too low serial
+    ('010190-000N',),  # Too low serial
+    ('000190-0023',),  # Invalid day
+    ('010090-002X',),  # Invalid month
+    ('010190-002r',),  # Invalid checksum
     ('101010-0102',),
     ('10a010-0101',),
     ('101010-0\xe401',),
@@ -43,3 +50,10 @@ def test_returns_true_on_valid_ssn(value):
 ])
 def test_returns_failed_validation_on_invalid_ssn(value):
     assert isinstance(fi_ssn(value), ValidationFailure)
+
+
+def test_returns_failed_validation_on_temporal_ssn_when_not_allowed():
+    assert isinstance(
+        fi_ssn('010190-900P', allow_temporal_ssn=False),
+        ValidationFailure
+    )
