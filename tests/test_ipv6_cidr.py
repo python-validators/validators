1c1543e4cd3fa782a15934eb9037a07581e2f1d4
William Woodruff <william@trailofbits.com>
2019-08-19 17:50:43 -0700

    tests: Fix tests

--- tests/test_ipv6_cidr.py
+++ tests/test_ipv6_cidr.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from validators import ipv4, ipv6, ValidationFailure
+from validators import ipv4_cidr, ipv6_cidr, ValidationFailure
 
 
-@pytest.mark.parametrize(('address',), [
+@pytest.mark.parametrize(('cidr',), [
     ('::1/0',),
     ('dead:beef:0:0:0:0:42:1/8',),
     ('abcd:ef::42:1/32',),
     ('0:0:0:0:0:ffff:1.2.3.4/64',),
     ('::192.168.30.2/128',),
 ])
-def test_returns_true_on_valid_ipv6_address(address):
-    assert ipv6(address)
-    assert not ipv4(address)
+def test_returns_true_on_valid_ipv6_cidr(cidr):
+    assert ipv6_cidr(cidr)
+    assert not ipv4_cidr(cidr)
 
 
-@pytest.mark.parametrize(('address',), [
+@pytest.mark.parametrize(('cidr',), [
     ('abc.0.0.1',),
     ('abcd:1234::123::1',),
     ('1:2:3:4:5:6:7:8:9',),
@@ -27,5 +27,5 @@ def test_returns_true_on_valid_ipv6_address(address):
     ('::1/-1',),
     ('::1/foo',),
 ])
-def test_returns_failed_validation_on_invalid_ipv6_address(address):
-    assert isinstance(ipv6(address), ValidationFailure)
+def test_returns_failed_validation_on_invalid_ipv6_cidr(cidr):
+    assert isinstance(ipv6_cidr(cidr), ValidationFailure)
