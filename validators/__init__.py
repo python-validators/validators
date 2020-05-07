4830da136b39dd04a61f886d191f499cd052add7
TimonPeng <timonminipeng@gmail.com>
2020-05-07 16:03:23 +0800

    Add bank card validations

--- validators/__init__.py
+++ validators/__init__.py
@@ -13,10 +13,21 @@ from .truthy import truthy
 from .url import url
 from .utils import ValidationFailure, validator
 from .uuid import uuid
-
+from .card import (
+    card_number,
+    visa,
+    mastercard,
+    amex,
+    unionpay,
+    diners,
+    jcb,
+    discover,
+)
 __all__ = ('between', 'domain', 'email', 'Max', 'Min', 'md5', 'sha1', 'sha224',
            'sha256', 'sha512', 'fi_business_id', 'fi_ssn', 'iban', 'ipv4',
            'ipv4_cidr', 'ipv6', 'ipv6_cidr', 'length', 'mac_address', 'slug',
-           'truthy', 'url', 'ValidationFailure', 'validator', 'uuid')
+           'truthy', 'url', 'ValidationFailure', 'validator', 'uuid',
+           'card_number', 'visa', 'mastercard', 'amex', 'unionpay', 'diners',
+           'jcb', 'discover')
 
 __version__ = '0.14.3'
