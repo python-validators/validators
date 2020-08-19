fd828101d63cffaca33b5ae6c5a6ece974319ab0
daveusa31 <bakteev2003@yandex.ru>
2020-08-19 21:34:23 +0300

    Added support bitcoin setwit-addresses validator

--- tests/test_btc_address.py
+++ tests/test_btc_address.py
@@ -7,6 +7,9 @@ from validators import btc_address, ValidationFailure
 @pytest.mark.parametrize(('address',), [
     ('17nuNm4QpgKuDvWy7Jh2AZ2nzZpMyKSKzT',),
     ('3Cwgr2g7vsi1bXDUkpEnVoRLA9w4FZfC69',),
+    ('bc1q2ve3m49wyvqnj8mhuk9gqm2jep8gh5cr59vk6j',),
+    ('bc1qe3qwtl0m5m8wnzv2w3cmvv4ug4tqenwsfqdaas',),
+    ('bc1qxutdwh78302u57x0psad869zwgzj7uxtmvr3j5',),
 ])
 def test_returns_true_on_valid_mac_address(address):
     assert btc_address(address)
--- validators/btc_address.py
+++ validators/btc_address.py
@@ -2,7 +2,13 @@ import re
 
 from .utils import validator
 
-pattern = re.compile(r"^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$")
+SegWit_expression = r"\bbc(0([ac-hj-np-z02-9]{39}|[ac-hj-np-z02-9]{59})"
+SegWit_expression += r"|1[ac-hj-np-z02-9]{8,87})\b$"
+
+patterns = {
+    "P2PKH": re.compile(r"^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$"),
+    "SegWit": re.compile(SegWit_expression),
+}
 
 
 @validator
@@ -20,4 +26,9 @@ def btc_address(value):
 
     :param value: Bitcoin address string to validate
     """
-    return pattern.match(value)
+    response = False
+    for _, pattern in patterns.items():
+        if pattern.match(value):
+            response = True
+
+    return response
