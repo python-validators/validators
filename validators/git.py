e323a1070681db2c9dc5cefa0b72063142f4de4d
Adam Lev-Libfeld <adam.inf@gmail.com>
2022-07-19 23:11:42 +0300

    cleanup and prepare for PR

--- validators/git.py
+++ validators/git.py
@@ -2,7 +2,13 @@ import re
 
 from .utils import validator
 
-
+"""
+sources:
+https://github.com/git/git/blob/master/url.c
+https://git-scm.com/docs/git-clone
+https://stackoverflow.com/questions/23976019/how-to-verify-valid-format-of-url-as-a-git-repo/60000569#comment111902087_60000569
+https://github.com/jonschlinkert/is-git-url
+"""
 url_regex = re.compile(
     # protocol
     # either prot:// (protocol captured as group 1) or git@serv (server captured as group 2)
@@ -31,23 +37,27 @@ def git(value: str, strict: bool = False):
 
     Examples::
 
-        >>> git('git@bitbucket.org:username/reponame.git')
+        >>> git('git@github.org:username/reponame.git')
         True
 
-        >>> git('http://192.168.1.127/user/project')
+        >>> git('git@github.com:username/reponame')
         True
 
-        >>> git('git://github.com/somthing/somthing.git#ff786f9f')
+        >>> git('http://192.168.1.127/user/project.git')
+        True
+
+        >>> git('git://bitbucket.org/org/repo.git#ff786f9f')
         True
 
         >>> git('https://www.github.com')
         ValidationFailure(func=git, ...)
 
-        >>> git('http://192.168.1.127/user/project', strict=True)
+        >>> git('git@github.com:username/reponame', strict=True)
         ValidationFailure(func=git, ...)
 
     :param value: URL address string to validate
-    :param strict: (default=False) enfocre URL end with .git
+    :param strict: (default=False) enfocre URL end with .git (if false, upon validation failure '.git; is added
+    to the end if the procided value and validation retried)
     """
     result = url_regex.match(value)
 
@@ -58,75 +68,3 @@ def git(value: str, strict: bool = False):
         value = value[:-1] if value.endswith('/') else value + ".git"
         result = url_regex.match(value)
         return result
-
-'''
-git@bitbucket.org:username/reponame.git
-https://username@bitbucket.org/otherusername/reponame.git
-https://github.com/username/reponame.git
-git@github.com:username/reponame.git
-git@gitlab.com:groupname/reponame.git
-https://gitlab.com/groupname/reponame.git
-git@bitbucket.org:workspace/reponame.git
-https://username@bitbucket.org/workspace/reponame.git
-https://bitbucket.org/workspace/reponame.git
-git@gitlab.com:username/reponame.git
-https://gitlab.com/username/reponame.git
-git://github.com/somthing/somthing.git#ff786f9f
-git://github.com/somthing/somthing.git#gh-pages
-git://github.com/somthing/somthing.git#master
-git://github.com/somthing/somthing.git#Quick-Fix
-git://github.com/somthing/somthing.git#quick_fix
-git://github.com/somthing/somthing.git#v0.1.0
-git://host.xz/path/to/repo.git/
-git://host.xz/~user/path/to/repo.git/
-git@192.168.101.127:user/project.git
-git@github.com:user/project.git
-git@github.com:user/some-project.git
-git@github.com:user/some-project.git
-git@github.com:user/some_project.git
-git@github.com:user/some_project.git
-http://github.com/user/project.git
-http://host.xz/path/to/repo.git/
-https://github.com/user/project.git
-https://host.xz/path/to/repo.git/
-https://username::;*%$:@github.com/username/repository.git
-https://username:$fooABC@:@github.com/username/repository.git
-https://username:password@github.com/username/repository.git
-ssh://host.xz/path/to/repo.git/
-ssh://host.xz/path/to/repo.git/
-ssh://host.xz/~/path/to/repo.git
-ssh://host.xz/~user/path/to/repo.git/
-ssh://user@host.xz/path/to/repo.git/
-ssh://user@host.xz/path/to/repo.git/
-ssh://user@host.xz/~/path/to/repo.git
-ssh://user@host.xz/~user/path/to/repo.git/
-ssh://user@host.xz:port/path/to/repo.git/
-https://username@bitbucket.org:8080/otherusername/reponame.git
-ssh://host.xz:port/path/to/repo.git/
-ssh://host.xz:1234/path/to/repo.git/
-https://192.168.1.127/user/project.git
-http://192.168.1.127/user/project.git
-
-
-https://www.github.com
-git@bitbucket.org:username/repo name.git
-https://gitlab.com/username/reponame.git -b branch
-https://gitlab.com/username/reponame.git --flag
-https://gitlab.com/username/reponame.git \-f
-/path/to/repo.git/
-file:///path/to/repo.git/
-file://~/path/to/repo.git/
-git@github.com:user/some_project.git/foo
-git@github.com:user/some_project.gitfoo
-host.xz:/path/to/repo.git/
-host.xz:path/to/repo.git
-host.xz:~user/path/to/repo.git/
-path/to/repo.git/
-rsync://host.xz/path/to/repo.git/
-user@host.xz:/path/to/repo.git/
-user@host.xz:path/to/repo.git
-user@host.xz:~user/path/to/repo.git/
-~/path/to/repo.git
-git@bitbucket.org:username/reponame.gi
-
-'''
