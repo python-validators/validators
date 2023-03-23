e612c6ecddf915d19b8c16e2681ed99229394fe7
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2023-03-23 16:53:07 +0530

    feat: add build for pypi workflow
    - adds GitHub workflow to build for PyPI
    - adds a convenience script to build package
    - `isort`'s first-party is now annotated as `local`
    - minor modifications made to `docs/gen_docs.py`

---
+++ build.py
@@ -0,0 +1,16 @@
+"""Remove Refs."""
+
+# standard
+from subprocess import run
+from shutil import rmtree
+from pathlib import Path
+
+# local
+from docs.gen_docs import generate_documentation
+
+if __name__ == "__main__":
+    project_dir = Path(__file__).parent
+    generate_documentation(project_dir, discard_refs=False)
+    process = run(("poetry", "build"), capture_output=True)
+    print(process.stderr.decode() + process.stdout.decode())
+    rmtree(project_dir / "docs/reference", ignore_errors=True)
