5592922ff6d22c1e5971d6cc537a15fccf1953e2
Mehdi Samsami <mehdisamsami@live.com>
2023-06-12 13:38:06 +0330

    move trx validator to a submodule

---
+++ validators/crypto_addresses/__init__.py
@@ -0,0 +1,10 @@
+"""Crypto addresses."""
+# -*- coding: utf-8 -*-
+
+# isort: skip_file
+
+# local
+from .eth_address import eth_address
+from .trx_address import trx_address
+
+__all__ = ("eth_address", "trx_address")
