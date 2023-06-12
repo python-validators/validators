80c8fb930bafced0f21945f2c38a85857cdf73a5
Mehdi Samsami <mehdisamsami@live.com>
2023-06-12 15:26:55 +0330

    move btc estimator and its test to crypto addresses submodules

--- validators/crypto_addresses/__init__.py
+++ validators/crypto_addresses/__init__.py
@@ -4,7 +4,8 @@
 # isort: skip_file
 
 # local
+from .btc_address import btc_address
 from .eth_address import eth_address
 from .trx_address import trx_address
 
-__all__ = ("eth_address", "trx_address")
+__all__ = ("btc_address", "eth_address", "trx_address")
