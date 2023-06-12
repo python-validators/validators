9345ae4489c934a59367008a62e604a37ea28f19
Mehdi Samsami <mehdisamsami@live.com>
2023-06-12 15:19:27 +0330

    remove btc validator from init

--- validators/crypto_addresses/__init__.py
+++ validators/crypto_addresses/__init__.py
@@ -4,8 +4,7 @@
 # isort: skip_file
 
 # local
-from .btc_address import btc_address
 from .eth_address import eth_address
 from .trx_address import trx_address
 
-__all__ = ("btc_address", "eth_address", "trx_address")
+__all__ = ("eth_address", "trx_address")
