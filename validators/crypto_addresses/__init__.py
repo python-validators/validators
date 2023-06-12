ec1071f90f0462dd596bda7ffa85d999397e5d05
Mehdi Samsami <mehdisamsami@live.com>
2023-06-12 13:54:22 +0330

    add trx validator to init

--- validators/crypto_addresses/__init__.py
+++ validators/crypto_addresses/__init__.py
@@ -6,5 +6,6 @@
 # local
 from .btc_address import btc_address
 from .eth_address import eth_address
+from .trx_address import trx_address
 
-__all__ = ("btc_address", "eth_address")
+__all__ = ("btc_address", "eth_address", "trx_address")
