875dcd351d05ef68c116f935d2eb00509209d1a9
Mehdi Samsami <mehdisamsami@live.com>
2023-06-12 13:48:22 +0330

    add init files for new submodules

---
+++ validators/crypto_addresses/__init__.py
@@ -0,0 +1,10 @@
+"""Crypto addresses."""
+# -*- coding: utf-8 -*-
+
+# isort: skip_file
+
+# local
+from .btc_address import btc_address
+from .eth_address import eth_address
+
+__all__ = ("btc_address", "eth_address")
