56f02106b0b5ee01b7219dddfc928ff83d79cdcf
a.shilov <tabbols95@gmail.com>
2024-10-27 02:58:27 +0300

    Validators inn company

--- src/validators/inn.py
+++ src/validators/inn.py
@@ -1,8 +1,29 @@
 """Inn."""
 
-from .utils import validator
+# from .utils import validator
 
 
-@validator
+# @validator
 def inn(value: str):
-    pass
+    """Description"""
+    if not value:
+        return False
+
+    try:
+        digits = list(map(int, value))
+        # person
+        if len(digits) == 10:
+            weight_coefs = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
+            control_number = sum([d * w for d, w in zip(digits, weight_coefs)]) % 11
+            return (control_number % 10) == digits[-1] if control_number > 9 else control_number == digits[-1]
+        # company
+        elif len(digits) == 12:
+            pass
+        # error inn
+        else:
+            return False
+    except ValueError:
+        return False
+
+if "__main__" == __name__:
+    print(inn('5260355389'))
