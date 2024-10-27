b4f1cb9bb0fbce54a89bcc253e32a10b6cfe145a
a.shilov <tabbols95@gmail.com>
2024-10-27 03:18:03 +0300

    validators inn person

--- src/validators/inn.py
+++ src/validators/inn.py
@@ -11,21 +11,20 @@ def inn(value: str):
 
     try:
         digits = list(map(int, value))
-        # person
+        # company
         if len(digits) == 10:
             weight_coefs = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0]
             control_number = sum([d * w for d, w in zip(digits, weight_coefs)]) % 11
             return (control_number % 10) == digits[-1] if control_number > 9 else control_number == digits[-1]
-        # company
+        # person
         elif len(digits) == 12:
             weight_coefs1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 6, 0, 0]
+            control_number1 = sum([d * w for d, w in zip(digits, weight_coefs1)]) % 11
             weight_coefs2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 0, 0, 0]
-            pass
-        # error inn
+            control_number2 = sum([d * w for d, w in zip(digits, weight_coefs2)]) % 11
+            return ((control_number1 % 10) == digits[-2] if control_number1 > 9 else control_number1 == digits[-2] and
+                    (control_number2 % 10) == digits[-1] if control_number2 > 9 else control_number2 == digits[-1])
         else:
             return False
     except ValueError:
         return False
-
-if "__main__" == __name__:
-    print(inn('5260355389'))
